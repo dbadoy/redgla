bf2f36c4eab4d9e49f5b2a9296a6c318f909b447
dbadoy4874 <dbadoy4874@gmail.com>
2023-02-16 17:10:20 +0900

    config: init

---
+++ config.go
@@ -0,0 +1,73 @@
+package redgla
+
+import (
+	"errors"
+	"fmt"
+	"net/url"
+	"time"
+)
+
+const (
+	defaultThreshold         = 100
+	defaultHeartbeatInterval = 3 * time.Second
+	defaultHeartbeatTimeout  = time.Second
+)
+
+var (
+	errInvalidInterval = errors.New("invalid heartbeat interval")
+	errInvalidTimeout  = errors.New("invalid heartbeat timeout")
+	errInvalidEndpoint = errors.New("invalid endpoint")
+)
+
+type Config struct {
+	// A list of endpoints to send batch requests to.
+	Endpoints []string
+
+	// Threshold to send a batch request. If the number of requests is
+	// greater than the value, they are converted to batch requests.
+	Threshold int
+
+	// Ping interval for checks alive endpoints.
+	HeartbeatInterval time.Duration
+
+	// Timeout for requests to determine 'alive'.
+	HeartbeatTimeout time.Duration
+}
+
+func DefaultConfig() *Config {
+	return &Config{
+		Threshold:         defaultThreshold,
+		HeartbeatInterval: defaultHeartbeatInterval,
+		HeartbeatTimeout:  defaultHeartbeatTimeout,
+	}
+}
+
+func (c *Config) validate() error {
+	if len(c.Endpoints) == 0 {
+		return errInvalidEndpoint
+	}
+
+	for _, endpoint := range c.Endpoints {
+		if err := validateEndpoint(endpoint); err != nil {
+			return err
+		}
+	}
+
+	if c.HeartbeatInterval == 0 {
+		return errInvalidInterval
+	}
+
+	if c.HeartbeatTimeout == 0 {
+		return errInvalidTimeout
+	}
+
+	return nil
+}
+
+func validateEndpoint(endpoint string) error {
+	if _, err := url.ParseRequestURI(endpoint); err != nil {
+		return fmt.Errorf("%s: %w", endpoint, errInvalidEndpoint)
+	}
+
+	return nil
+}
---
+++ config_test.go
@@ -0,0 +1,84 @@
+package redgla
+
+import (
+	"errors"
+	"testing"
+	"time"
+)
+
+func TestDefaultConfig(t *testing.T) {
+	dcfg := DefaultConfig()
+
+	if dcfg.Threshold != defaultThreshold {
+		t.Fatalf("want: %v got: %v", defaultThreshold, dcfg.Threshold)
+	}
+
+	if dcfg.HeartbeatInterval != defaultHeartbeatInterval {
+		t.Fatalf("want: %v got: %v", defaultHeartbeatInterval, dcfg.HeartbeatInterval)
+	}
+
+	if dcfg.HeartbeatTimeout != defaultHeartbeatTimeout {
+		t.Fatalf("want: %v got: %v", defaultHeartbeatTimeout, dcfg.HeartbeatTimeout)
+	}
+}
+
+func TestConfigValidation(t *testing.T) {
+	tests := []struct {
+		cfg *Config
+		err error
+	}{
+		{
+			&Config{
+				Endpoints:         []string{"http://127.0.0.1:3821"},
+				Threshold:         1,
+				HeartbeatInterval: time.Second,
+				HeartbeatTimeout:  time.Second,
+			},
+			nil,
+		},
+		{
+			&Config{
+				Endpoints:         []string{"http://127.0.0.1:3821"},
+				Threshold:         100,
+				HeartbeatInterval: 0,
+				HeartbeatTimeout:  time.Second,
+			},
+			errInvalidInterval,
+		},
+		{
+			&Config{
+				Endpoints:         []string{"http://127.0.0.1:3821"},
+				Threshold:         100,
+				HeartbeatInterval: time.Second,
+				HeartbeatTimeout:  0,
+			},
+			errInvalidTimeout,
+		},
+		{
+			&Config{
+				Endpoints:         nil,
+				Threshold:         100,
+				HeartbeatInterval: time.Second,
+				HeartbeatTimeout:  time.Second,
+			},
+			errInvalidEndpoint,
+		},
+		{
+			&Config{
+				Endpoints:         []string{"http://127.0.0.1:3821", "dbadoy"},
+				Threshold:         100,
+				HeartbeatInterval: time.Second,
+				HeartbeatTimeout:  time.Second,
+			},
+			errInvalidEndpoint,
+		},
+	}
+
+	for _, test := range tests {
+		err := test.cfg.validate()
+
+		if !errors.Is(err, test.err) {
+			t.Fatalf("TestConfigValidation, want %v got %v", test.err, err)
+		}
+	}
+}
