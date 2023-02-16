a683524427957bd8504eca5cae4b8ad0cbb42fd5
dbadoy4874 <dbadoy4874@gmail.com>
2023-02-16 18:42:10 +0900

    beater: init

---
+++ beater.go
@@ -0,0 +1,139 @@
+package redgla
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+// HeartBeat is a method that can check whether the endpoint is working
+// or not. Since it is not possible to specify which service endpoint it
+// is, it is appropriately injected from the outside according to the usage.
+type HeartbeatFn func(ctx context.Context, endpoint string) error
+
+func DefaultHeartbeatFn(ctx context.Context, endpoint string) (*ethclient.Client, error) {
+	// context.WithTimeout has no effect on DialContext.
+	client, err := ethclient.DialContext(ctx, endpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	// It is recommended to make at least one rpc call.
+	_, err = client.ChainID(ctx)
+	return client, err
+}
+
+// Beater manages the status list by examining whether the endpoints
+// registered in the list are operating normally. The endpoint should be
+// URL format.
+type beater struct {
+	name      string
+	endpoints []string
+
+	mu sync.RWMutex
+
+	// Mapping 'member' to 'spent time'; for excluding too slow works nodes.
+	members map[string]time.Duration
+
+	quit chan struct{}
+
+	fn HeartbeatFn
+
+	interval time.Duration
+	timeout  time.Duration
+}
+
+type message struct {
+	endpoint string
+	spent    time.Duration
+}
+
+func newBeater(name string, endpoints []string, fn HeartbeatFn, interval, timeout time.Duration) (*beater, error) {
+	for _, endpoint := range endpoints {
+		if err := validateEndpoint(endpoint); err != nil {
+			return nil, err
+		}
+	}
+
+	return &beater{
+		name:      name,
+		endpoints: endpoints,
+		quit:      make(chan struct{}),
+		fn:        fn,
+		interval:  interval,
+		timeout:   timeout,
+	}, nil
+}
+
+func (b *beater) run() {
+	go b.loop()
+}
+
+func (b *beater) stop() {
+	b.quit <- struct{}{}
+	b.members = make(map[string]time.Duration)
+}
+
+func (b *beater) loop() {
+	timer := time.NewTimer(0)
+	defer timer.Stop()
+
+	for {
+		select {
+		case <-timer.C:
+			members := b.beat(b.endpoints)
+
+			b.mu.Lock()
+			b.members = members
+			b.mu.Unlock()
+
+			timer.Reset(b.interval)
+
+		case <-b.quit:
+			return
+		}
+	}
+}
+
+func (b *beater) beat(endpoints []string) map[string]time.Duration {
+	resc := make(chan *message, len(endpoints))
+
+	ctx, cancel := context.WithTimeout(context.Background(), b.timeout)
+	defer cancel()
+
+	start := time.Now()
+	for _, endpoint := range endpoints {
+		go func(t string) {
+			if err := b.fn(ctx, t); err != nil {
+				resc <- nil
+				return
+			}
+			resc <- &message{t, time.Since(start)}
+		}(endpoint)
+	}
+
+	m := make(map[string]time.Duration)
+
+	for i := 0; i < cap(resc); i++ {
+		msg := <-resc
+		if msg != nil {
+			m[msg.endpoint] = msg.spent
+		}
+	}
+
+	return m
+}
+
+func (b *beater) liveNodes() []string {
+	b.mu.RLock()
+	defer b.mu.RUnlock()
+
+	endpoints := make([]string, 0, len(b.members))
+	for e := range b.members {
+		endpoints = append(endpoints, e)
+	}
+
+	return endpoints
+}
---
+++ beater_test.go
@@ -0,0 +1,122 @@
+package redgla
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"time"
+)
+
+func TestDefaultHeartbeatFn(t *testing.T) {
+	tests := []struct {
+		endpoint string
+		succeed  bool
+	}{
+		{
+			"dbadoy",
+			false,
+		},
+		{
+			"https://rpc.ankr.com/eth",
+			true,
+		},
+		{
+			"https://rpc.flashbots.net",
+			true,
+		},
+	}
+
+	// Is context timeout works?
+	for _, test := range tests {
+		// Always return timeout error.
+		ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond)
+		defer cancel()
+
+		_, err := DefaultHeartbeatFn(ctx, test.endpoint)
+		if err == nil {
+			t.Fatalf("want: %v got: %v", test.succeed, false)
+		}
+	}
+
+	for _, test := range tests {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		_, err := DefaultHeartbeatFn(ctx, test.endpoint)
+		if err == nil && !test.succeed {
+			t.Fatalf("want: %v got: %v", test.succeed, err == nil)
+		}
+		if err != nil && test.succeed {
+			t.Fatalf("want: %v got: %v", test.succeed, err == nil)
+		}
+	}
+}
+
+func TestBeat(t *testing.T) {
+	// Always success.
+	fn := func(context.Context, string) error {
+		return nil
+	}
+
+	beater, err := newBeater("test", []string{"http://127.0.0.1:1823", "http://127.0.0.1:1824"}, fn, time.Second, time.Second)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	liveNodes := beater.beat(beater.endpoints)
+	if len(liveNodes) != 2 {
+		t.Fatalf("beater.beat failure, want: %v got: %v", 2, len(beater.liveNodes()))
+	}
+}
+
+func TestBeaterWithAlwaysSuccess(t *testing.T) {
+	// Always success.
+	fn := func(context.Context, string) error {
+		return nil
+	}
+
+	beater, err := newBeater("test", []string{"http://127.0.0.1:1823", "http://127.0.0.1:1824"}, fn, time.Second, time.Second)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	beater.run()
+
+	time.Sleep(10 * time.Millisecond)
+
+	if len(beater.liveNodes()) != 2 {
+		t.Fatalf("beater.beat failure, want: %v got: %v", 2, len(beater.liveNodes()))
+	}
+
+	beater.stop()
+
+	if len(beater.liveNodes()) != 0 {
+		t.Fatalf("beater.beat failure, want: %v got: %v", 0, len(beater.liveNodes()))
+	}
+}
+
+func TestBeaterWithAlwaysFail(t *testing.T) {
+	// Always success.
+	fn := func(context.Context, string) error {
+		return errors.New("no")
+	}
+
+	beater, err := newBeater("test", []string{"http://127.0.0.1:1823", "http://127.0.0.1:1824"}, fn, time.Second, time.Second)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	beater.run()
+
+	time.Sleep(10 * time.Millisecond)
+
+	if len(beater.liveNodes()) != 0 {
+		t.Fatalf("beater.beat failure, want: %v got: %v", 2, len(beater.liveNodes()))
+	}
+
+	beater.stop()
+
+	if len(beater.liveNodes()) != 0 {
+		t.Fatalf("beater.beat failure, want: %v got: %v", 0, len(beater.liveNodes()))
+	}
+}
