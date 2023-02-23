c8f0e0952bd00421b609e9345640c663d78dc388
dbadoy4874 <dbadoy4874@gmail.com>
2023-02-24 01:11:39 +0900

    example: add comments

--- example/main.go
+++ example/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	cfg := redgla.DefaultConfig()
 	cfg.Threshold = 5
-	cfg.Endpoints = append(cfg.Endpoints, "https://rpc.ankr.com/eth", "https://rpc.flashbots.net")
+	cfg.Endpoints = append(cfg.Endpoints, "https://rpc.ankr.com/eth", "https://eth.llamarpc.com", "https://api.securerpc.com/v1")
 
 	redgla, err := redgla.New(redgla.DefaultHeartbeatFn, cfg)
 	if err != nil {
@@ -21,18 +21,30 @@ func main() {
 
 	time.Sleep(3 * time.Second)
 
-	result, err := redgla.Benchmark(1000, 3)
-	if err != nil {
-		panic(err)
-	}
+	// // Benchmark
+	// result, err := redgla.Benchmark(1000, 3)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	fmt.Println(result)
+	// fmt.Println(result)
 
 	// This request is divided into 5 block request and sent to 2 nodes.
-	res, err := redgla.BlockByRangeWithBatch(30, 40)
+	n1 := time.Now()
+	res, err := redgla.BlockByRangeWithBatch(111, 120)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(res)
+	// 2023-02-24, Seoul: 1.928021s, 10
+	fmt.Println(time.Since(n1), len(res))
+
+	n2 := time.Now()
+	res, err = redgla.BlockByRange(171, 180)
+	if err != nil {
+		panic(err)
+	}
+
+	// 2023-02-24, Seoul: 3.542532459s, 10
+	fmt.Println(time.Since(n2), len(res))
 }
