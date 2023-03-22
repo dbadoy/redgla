999f5318058854e403a7de1f2a36e56a6ab0fde3
Seungbae Yu <dbadoy4874@gmail.com>
2023-03-22 23:10:46 +0900

    message: add content about license

--- message.go
+++ message.go
@@ -1,3 +1,8 @@
+// Copyright (c) 2023, redgla authors <dbadoy4874@gmail.com>
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 package redgla
 
 import (
